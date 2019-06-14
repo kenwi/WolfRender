446a488a69aba7f2520abe64aff02f35f2bafa46
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2019-06-14 09:21:35 +0200

    Use Run method in game class

--- src/Program.cs
+++ src/Program.cs
@@ -4,30 +4,9 @@
     {
         static void Main(string[] args)
         {
-            bool limited = false;
-            int targetfps = 60;
-            float targetUpdateRate = 1f / targetfps;
-
             Game.Instance.Init(1000, 700);
             InputHandler.Instance.Init();
-            while (Game.Instance.Window.IsOpen)
-            {
-                var dt = Game.Instance.DeltaTime;
-                if (limited)
-                {
-                    while (dt < targetUpdateRate)
-                    {
-                        dt += Game.Instance.DeltaTime;
-                        InputHandler.Instance.Update(dt);
-                    }
-                }
-                else
-                {
-                    InputHandler.Instance.Update(dt);
-                }
-                Game.Instance.Update(dt);
-                Game.Instance.Render();
-            }
+            Game.Instance.Run();
         }
     }
 }
