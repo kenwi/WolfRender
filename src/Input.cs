0c896d790b90e304877157c106f5c472f0d075e9
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2019-06-15 06:03:02 +0200

    Add basic mouse control

--- src/Input.cs
+++ src/Input.cs
@@ -8,49 +8,60 @@ namespace WolfRender
     public class Input : Singleton<Input>
     {
         List<Keyboard.Key> keydown = new List<Keyboard.Key>();
+        Vector2i previousMousePosition = Mouse.GetPosition();
+        float mouseRotationMultiplier = 0.1f;
 
         public void Init()
         {
+            // Game.Instance.Window.SetMouseCursorGrabbed(true);
+            // Game.Instance.Window.SetMouseCursorVisible(false);
             Game.Instance.Window.Closed += (s, e) => Game.Instance.Window.Close();
         }
 
         public void Update(float dt)
         {
             var player = Game.Instance.Player;
-            if (Keyboard.IsKeyPressed(Keyboard.Key.Left))
+            if (Keyboard.IsKeyPressed(Keyboard.Key.A))
             {
                 player.Direction -= player.RotationSpeed * dt;
             }
-            if (Keyboard.IsKeyPressed(Keyboard.Key.Right))
+
+            if (Keyboard.IsKeyPressed(Keyboard.Key.D))
             {
                 player.Direction += player.RotationSpeed * dt;
             }
-            if (Keyboard.IsKeyPressed(Keyboard.Key.Up))
+
+            if (Keyboard.IsKeyPressed(Keyboard.Key.W))
             {
                 var movementSpeed = player.MovementSpeed;
                 var playerDirection = player.Direction;
                 var position = new Vector2f(movementSpeed * MathF.Cos(playerDirection) * dt, movementSpeed * MathF.Sin(playerDirection) * dt);
                 player.Position += position;
             }
-            if (Keyboard.IsKeyPressed(Keyboard.Key.Down))
+
+            if (Keyboard.IsKeyPressed(Keyboard.Key.S))
             {
                 var movementSpeed = player.MovementSpeed;
                 var playerDirection = player.Direction;
                 var position = new Vector2f(movementSpeed * MathF.Cos(playerDirection) * dt, movementSpeed * MathF.Sin(playerDirection) * dt);
                 player.Position -= position;
             }
+
             if (Keyboard.IsKeyPressed(Keyboard.Key.PageUp))
             {
                 player.Fov += dt;
             }
+
             if (Keyboard.IsKeyPressed(Keyboard.Key.PageDown))
             {
                 player.Fov -= dt;
             }
+
             if (Keyboard.IsKeyPressed(Keyboard.Key.Escape))
             {
                 Game.Instance.Window.Close();
             }
+
             if (Keyboard.IsKeyPressed(Keyboard.Key.H))
             {
                 if (!keydown.Contains(Keyboard.Key.H))
@@ -76,6 +87,24 @@ namespace WolfRender
             {
                 keydown.Remove(Keyboard.Key.L);
             }
+
+            var mousePosition = Mouse.GetPosition();
+            var mouseDelta = mousePosition - previousMousePosition;
+            if (MathF.Sqrt(mouseDelta.X * mouseDelta.X + mouseDelta.Y * mouseDelta.Y) > 0)
+            {
+                player.Direction += player.RotationSpeed * mouseRotationMultiplier * mouseDelta.X * dt;
+            }
+            if (mousePosition.X + 50 > Game.Instance.Window.Size.X + Game.Instance.Window.Position.X)
+            {
+                mousePosition.X -= (int)Game.Instance.Window.Size.X / 2;
+                Mouse.SetPosition(new Vector2i(mousePosition.X, mousePosition.Y));
+            }
+            if (mousePosition.X - 50 <= Game.Instance.Window.Position.X)
+            {
+                mousePosition.X += (int)Game.Instance.Window.Size.X / 2;
+                Mouse.SetPosition(new Vector2i(mousePosition.X, mousePosition.Y));
+            }
+            previousMousePosition = mousePosition;
         }
     }
 }
