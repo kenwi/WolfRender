c3d86267807cfba3906cff28f827c091c7a6a68b
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2019-06-15 16:49:12 +0200

    Add left right rotation

--- src/Input.cs
+++ src/Input.cs
@@ -9,11 +9,12 @@ namespace WolfRender
     {
         List<Keyboard.Key> keydown = new List<Keyboard.Key>();
         Vector2i previousMousePosition = Mouse.GetPosition();
-        float mouseRotationMultiplier = 0.1f;
+        float mouseRotationMultiplier = 0.2f;
 
         public void Init()
         {
-            Game.Instance.Window.SetMouseCursorVisible(false);
+            // Game.Instance.Window.SetMouseCursorVisible(false);
+            Game.Instance.Window.SetMouseCursorGrabbed(true);
             Game.Instance.Window.Closed += (s, e) => Game.Instance.Window.Close();
         }
 
@@ -112,42 +113,29 @@ namespace WolfRender
 
         private void PlayerRotation(float dt, Player player)
         {
+            if (Keyboard.IsKeyPressed(Keyboard.Key.Right))
+            {
+                player.Direction += player.RotationSpeed * mouseRotationMultiplier * 5 * dt;
+            }
+            if (Keyboard.IsKeyPressed(Keyboard.Key.Left))
+            {
+                player.Direction -= player.RotationSpeed * mouseRotationMultiplier * 5 * dt;
+            }
             var mousePosition = Mouse.GetPosition();
             var mouseDelta = mousePosition - previousMousePosition;
-            if (MathF.Sqrt(mouseDelta.X * mouseDelta.X) > 0)
+            if (mouseDelta.X != 0)
             {
                 player.Direction += player.RotationSpeed * mouseRotationMultiplier * mouseDelta.X * dt;
             }
 
             if (!Game.Instance.MouseVisible)
             {
-                if (mousePosition.X> Game.Instance.Window.Size.X + Game.Instance.Window.Position.X)
+                if (mousePosition.X <= Game.Instance.Window.Position.X - 1 || mousePosition.X >= Game.Instance.Window.Position.X + Game.Instance.Window.Size.X)
                 {
-                    mousePosition.X -= (int)Game.Instance.Window.Size.X / 2;
-                    var position = new Vector2i(mousePosition.X, mousePosition.Y);
-                    previousMousePosition = position;
-                    Mouse.SetPosition(position);
-                }
-                if (mousePosition.X<= Game.Instance.Window.Position.X)
-                {
-                    mousePosition.X += (int)Game.Instance.Window.Size.X / 2;
-                    var position = new Vector2i(mousePosition.X, mousePosition.Y);
-                    previousMousePosition = position;
-                    Mouse.SetPosition(position);
-                }
-                if (mousePosition.Y> Game.Instance.Window.Size.Y + Game.Instance.Window.Position.Y)
-                {
-                    mousePosition.Y -= (int)Game.Instance.Window.Size.Y / 2;
-                    var position = new Vector2i(mousePosition.X, mousePosition.Y);
-                    previousMousePosition = position;
-                    Mouse.SetPosition(position);
-                }
-                if (mousePosition.Y<= Game.Instance.Window.Position.Y)
-                {
-                    mousePosition.Y += (int)Game.Instance.Window.Size.Y / 2;
-                    var position = new Vector2i(mousePosition.X, mousePosition.Y);
-                    previousMousePosition = position;
-                    Mouse.SetPosition(position);
+                    var center = new Vector2i((int)VideoMode.DesktopMode.Width / 2, (int)VideoMode.DesktopMode.Height / 2);
+                    previousMousePosition = center;
+                    Mouse.SetPosition(center);
+                    return;
                 }
             }
             previousMousePosition = mousePosition;
