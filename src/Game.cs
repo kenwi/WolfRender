dc2cc69dc872e4df885a0872c070e5abd0a8db64
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2019-06-19 09:34:00 +0200

    More sensible framerate limiting

--- src/Game.cs
+++ src/Game.cs
@@ -14,18 +14,33 @@ namespace WolfRender
         Time totalGameTime;
         RenderWindow window;
         float deltaTime;
-        float targetFps;
+        uint targetFps;
         bool mouseVisible;
         bool limited;
 
-        public RenderWindow Window { get => window; private set => window = value; }
+        public RenderWindow Window { get => window; set => window = value; }
         public Vector2i WindowCenter => window.Position + new Vector2i((int)window.Size.X / 2, (int)window.Size.Y / 2);
         public Time TotalGameTime { get => totalGameTime; }
         public Player Player { get => player; set => player = value; }
         public Random Random { get; private set; }
         public float DeltaTime { get => deltaTime; set => deltaTime = value; }
-        public bool HelpMenuVisible { get; set; }
-        public bool FramerateLimited { get => limited; set => limited = value; }
+        public bool IsHelpMenuVisible { get; set; }
+        public bool IsFullScreen { get; set; }
+
+        public bool IsFramerateLimited
+        {
+            get => limited;
+            set
+            {
+                uint fps = 0;
+                if(value)
+                {
+                    fps = targetFps;
+                }
+                window.SetFramerateLimit(fps);
+                limited = value;
+            }
+        }
 
         public bool MouseVisible
         {
@@ -38,19 +53,19 @@ namespace WolfRender
             }
         }
 
-        public void Init(uint width, uint height, int targetFps = 60, bool limitFrameRate = true)
+        public void Init(uint width, uint height, uint targetFps = 60, bool limitFrameRate = true)
         {
-            this.targetFps = targetFps;
             Random = new Random();
             gameTime = new Clock();
             window = new RenderWindow(new VideoMode(width, height, VideoMode.DesktopMode.BitsPerPixel), "WolfRender");
+            this.targetFps = targetFps;
+            this.IsFramerateLimited = limitFrameRate;
             map = new Map();
             player = new Player();
             player.Fov = MathF.PI * 0.5f;
             player.Position = new Vector2f(map.Size.X / 4, map.Size.Y / 4);
             player.RotationSpeed = Tools.DegToRad(100);
             player.MovementSpeed = 2;
-            limited = limitFrameRate;
             effects = new Effect[]{
                 new MapRenderer(map, player)
                 , new HelpScreen()
@@ -63,18 +78,11 @@ namespace WolfRender
             while (window.IsOpen)
             {
                 deltaTime = gameTime.Restart().AsSeconds();
-                if (limited)
-                {
-                    while (deltaTime < 1f / targetFps)
-                    {
-                        deltaTime += gameTime.Restart().AsSeconds();
-                    }
-                }
                 window.DispatchEvents();
                 Input.Instance.Update(deltaTime);
                 Update(deltaTime);
                 Render();
-                totalGameTime += Time.FromSeconds(deltaTime);                
+                totalGameTime += Time.FromSeconds(deltaTime);
             }
         }
 
