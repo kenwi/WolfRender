dbb9249ac35dc9f975130946724d14ef03400854
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2025-02-20 19:50:51 +0100

    refactor: use dependency injection pattern

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using WolfRender.Interfaces;
+using WolfRender.Services;
+
+namespace WolfRender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Game.Instance.Init(1920, 1080);
+            //Input.Instance.Init();
+            //Game.Instance.Run();
+
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddSingleton<IPlayerService, PlayerService>();
+                    services.AddSingleton<ITextureService, TextureService>();
+                    services.AddSingleton<IGameService, GameService>();
+                    services.AddSingleton<IMapRendererService, TexturedMapRendererService>();
+                    services.AddSingleton<IMapService, MapService>();
+
+                    services.AddHostedService<WindowService>();
+                })
+                .Build()
+                .Run();
+        }
+    }
+}
